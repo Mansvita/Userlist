3e2a041ec774339b5dcb6e754bde9812071eea83
Mansvita <152044292+Mansvita@users.noreply.github.com>
2024-05-15 15:54:50 -0400

    Update secondvc.swift

--- tableviewuserlist/Controller/secondvc.swift
+++ tableviewuserlist/Controller/secondvc.swift
@@ -1,9 +1,3 @@
-//
-//  secondvc.swift
-//  tableviewuserlist
-//
-//  Created by MANSVITA on 05/09/22.
-//
 
 import UIKit
 
@@ -15,16 +9,5 @@ class secondvc: UIViewController {
         self.navigationItem.title = "NouserlistDetail"
         // Do any additional setup after loading the view.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
